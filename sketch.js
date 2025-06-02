e00e0f40a5060645e50e5569496ace8611026426
012michel <michel.fiuza@escola.pr.gov.br>
2025-06-02 09:14:20 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,19 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+  background("white");
 }
-
-
 function draw() {
-  background(220);
+  fill("green");
+  rect(0, 0, 100, 200);
+  fill("white");
+  rect(100, 0, 100, 200);
+  fill("red");
+  rect(200, 0, 100, 200);
+
+  fill("pink");
+  rect(0, 250, 300, 100);
+  fill("red");
+  rect(0, 350, 300, 100);
+  fill("yellow");
+  rect(0, 450, 300, 100);
 }
